63bb101a06bc7b7bd4e8e27aea906557740f9696
hikarido <tocin723@gmail.com>
2020-04-15 00:55:20 +0300

    Get started HW 2. Initial commit

---
+++ hw02-DIYArray/src/main/java/ru/otus/hw02/DIYArray.java
@@ -0,0 +1,8 @@
+package ru.otus.hw02;
+
+public class DIYArray{
+	public static void main(String[] args){
+		System.out.println("Hello DIY");
+
+	}
+}
---
+++ hw02-DIYArray/src/test/java/ru/otus/hw2/DIYArrayTest.java
@@ -0,0 +1,11 @@
+package ru.otus.hw2;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DIYArrayTest {
+    @Test
+    public void FirstTest(){
+        assertEquals(1, 1);
+    }
+}
