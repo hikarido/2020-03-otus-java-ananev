8d691b0cc867f2995ec6b1ea914e65a7a55df2d9
hikarido <tocin723@gmail.com>
2020-05-08 04:47:12 +0300

    cmd args were parsed

--- hw3-own-test-framework/src/main/java/ru/otus/hw3/ConsoleLauncher.java
+++ hw3-own-test-framework/src/main/java/ru/otus/hw3/ConsoleLauncher.java
@@ -1,14 +1,29 @@
 package ru.otus.hw3;
 
 
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
+import java.util.logging.Logger;
+import ru.otus.hw3.TestRunner;
 
 /**
  * Command line tool for test running
  */
 public class ConsoleLauncher {
     public static void main(String[] args) {
-        System.out.println("I am the test framework");
         Options cliOptions = new Options();
+        cliOptions.addRequiredOption("i", "input-test-path", true, "Path to file which contains test");
+
+        HelpFormatter help = new HelpFormatter();
+
+        CommandLineParser parser = new DefaultParser();
+
+        try{
+            CommandLine cli = parser.parse(cliOptions, args);
+            String pathToTest = cli.getOptionValue("i");
+            new TestRunner(pathToTest);
+        }
+        catch (ParseException e){
+            help.printHelp("Runs all tests which presents in file passed as -i arg", cliOptions);
+        }
     }
 }
