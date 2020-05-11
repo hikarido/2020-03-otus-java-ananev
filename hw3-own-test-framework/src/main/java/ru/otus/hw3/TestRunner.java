dc5a3dd365dbb93b279d0ddbb3fc445bd4e82241
hikarido <tocin723@gmail.com>
2020-05-11 14:18:55 +0300

    instance of test was created

--- hw3-own-test-framework/src/main/java/ru/otus/hw3/TestRunner.java
+++ hw3-own-test-framework/src/main/java/ru/otus/hw3/TestRunner.java
@@ -1,9 +1,14 @@
 package ru.otus.hw3;
 
+import com.sun.jdi.InvocationException;
+
 import java.lang.Class;
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * Explores one class on presence of tests
@@ -15,8 +20,24 @@ public class TestRunner {
 
     public TestRunner(String name){
         Class<?> inspectingClazz = loadClassDescriptorByName(name);
+        ArrayList<Method> testMethods = new ArrayList<>();
+        ArrayList<Method> beforeMethods = new ArrayList<>();
+        ArrayList<Method> afterMethods = new ArrayList<>();
+
         if(inspectingClazz != null){
-            Constructor<?> constructor = getDefaultConstructor(inspectingClazz);
+            Object test = instantiateClass(inspectingClazz);
+
+        }
+    }
+
+    private Object instantiateClass(Class<?> inspectingClazz) {
+        Constructor<?> constructor = getDefaultConstructor(inspectingClazz);
+        try {
+            Object test = constructor.newInstance();
+            return test;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            logger.log(Level.SEVERE, e.toString());
+            throw  new RuntimeException("Can't instantiate class", e);
         }
     }
 
