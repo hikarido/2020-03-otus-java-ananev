9ab9455407811b02582a3290cd9c666ec708886d
hikarido <tocin723@gmail.com>
2020-05-11 10:59:20 +0300

    reflection experiments

--- hw3-own-test-framework/src/main/java/ru/otus/hw3/TestRunner.java
+++ hw3-own-test-framework/src/main/java/ru/otus/hw3/TestRunner.java
@@ -1,19 +1,71 @@
 package ru.otus.hw3;
 
 import java.lang.Class;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.reflect.Constructor;
 
 /**
  * Explores one class on presence of tests
  * And executes them
  */
 public class TestRunner {
+
+    private final Logger logger = Logger.getLogger(this.getClass().getCanonicalName());
+
     public TestRunner(String name){
+        Class<?> inspectingClazz = loadClassDescriptorByName(name);
+        if(inspectingClazz != null){
+            Constructor<?> constructor = getDefaultConstructor(inspectingClazz);
+        }
+    }
+
+    private Class<?> loadClassDescriptorByName(String name){
         try{
-            Class clazz = Class.forName(name);
+            Class<?> clazz = Class.forName(name);
+            logger.log(Level.INFO, "Class " + clazz.getSimpleName() + " was loaded");
+            return clazz;
         }
         catch(ClassNotFoundException e){
-            Logger.getLogger(this.getClass().getCanonicalName()).severe(e.toString());
+            logger.log(Level.INFO, e.toString());
+            return null;
+        }
+    }
+
+    private <T> Constructor<T> getDefaultConstructor(Class<T> clazz){
+        try {
+            Constructor<T> constructor = clazz.getConstructor();
+            return constructor;
+        } catch (NoSuchMethodException e) {
+            RuntimeException haveNoDefaultConstructor;
+            haveNoDefaultConstructor = new RuntimeException(
+                    "Class " + clazz.getSimpleName() + " have no default constructor",
+                    e
+            );
+            throw haveNoDefaultConstructor;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hw3-own-test-framework/src/test/java/TestRunnerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import ru.otus.hw3.TestRunner;
+
+public class TestRunnerTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void runBoringRocketScienceBusinessLogicTest(){
+        new TestRunner("BoringRocketScienceBusinessLogicTest");
+    }
+}
