9b75aee1be6cf8eba305b42cbe330720b758465a
hikarido pechka <tocin723@gmail.com>
2020-06-07 04:02:53 +0300

    hw8 first look(pure at main)

---
+++ hw08-json-object-writer/src/test/java/hw08/mygson/SubObject.java
@@ -0,0 +1,14 @@
+package hw08.mygson;
+
+public class SubObject{
+    public int valueInt = 3;
+    public int defaultValueInt;
+    public String empty = null;
+    public boolean bool = false;
+    public String str = "Hello";
+    private String privateStr = "HelloPrivateStr";
+    private int privateInt = 4;
+    private int privateefaultInt;
+    private Integer objInteger = 100;
+    private int[] ints = {1,2,3,4,5,6};
+}
