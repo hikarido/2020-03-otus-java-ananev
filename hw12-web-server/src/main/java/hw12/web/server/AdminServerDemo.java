281bf3231e72e28fd1163664175fe54e4732b8be
Ananev Konstantin <ananev@protei.ru>
2020-10-23 04:03:23 +0300

    security handling of admin page and api was added

--- hw12-web-server/src/main/java/hw12/web/server/AdminServerDemo.java
+++ hw12-web-server/src/main/java/hw12/web/server/AdminServerDemo.java
@@ -2,9 +2,11 @@ package hw12.web.server;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
 import org.hibernate.SessionFactory;
 import ru.otus.core.dao.UserDao;
-import ru.otus.core.sessionmanager.SessionManager;
+import ru.otus.helpers.FileSystemHelper;
 import ru.otus.hibernate.dao.UserDaoHibernate;
 import ru.otus.hibernate.sessionmanager.SessionManagerHibernate;
 import ru.otus.server.AdminServer;
@@ -18,6 +20,8 @@ public class AdminServerDemo{
     private static final int WEB_SERVER_PORT = 8080;
     private static final String TEMPLATES_DIR = "/templates/";
     private static final String HIBERNATE_CONFIG = "hibernate.cfg.xml";
+    private static final String HASH_LOGIN_SERVICE_CONFIG_NAME = "realm.properties";
+    private static final String REALM_NAME = "AnyRealm";
 
     public static void main(String[] args) throws Exception {
         SessionFactory sessionFactory = HibernateUtils.buildSessionFactory(
@@ -32,7 +36,11 @@ public class AdminServerDemo{
         Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
         TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
 
-        UsersWebServer adminServer = new AdminServer(WEB_SERVER_PORT, userDao, gson, templateProcessor);
+        String hashLoginServiceConfigPath
+                = FileSystemHelper.localFileNameOrResourceNameToFullPath(HASH_LOGIN_SERVICE_CONFIG_NAME);
+        LoginService loginService = new HashLoginService(REALM_NAME, hashLoginServiceConfigPath);
+
+        UsersWebServer adminServer = new AdminServer(loginService, WEB_SERVER_PORT, userDao, gson, templateProcessor);
 
         adminServer.start();
         adminServer.join();
--- hw12-web-server/src/main/java/ru/otus/server/AdminServer.java
+++ hw12-web-server/src/main/java/ru/otus/server/AdminServer.java
@@ -1,46 +1,54 @@
 package ru.otus.server;
 
 import com.google.gson.Gson;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.security.Constraint;
 import ru.otus.core.dao.UserDao;
 import ru.otus.helpers.FileSystemHelper;
 import ru.otus.services.TemplateProcessor;
 import ru.otus.servlet.AdminTerminalServlet;
 import ru.otus.servlet.UsersApiServlet;
 
-public class AdminServer implements UsersWebServer{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AdminServer implements UsersWebServer {
 
     private static final String START_PAGE_NAME = "index.html";
     private static final String COMMON_RESOURCES_DIR = "static";
     private static final String USER_API_CONTEXT_PATH = "/api/user/*";
-    private static final String  ADMIN_TERMINAL_CONTEXT_PATH = "/adminTerminal";
+    private static final String ADMIN_TERMINAL_CONTEXT_PATH = "/adminTerminal";
+    private static final String ROLE_NAME_ADMIN = "admin";
+    private static final String CONSTRAINT_NAME = "auth";
 
     private final UserDao userDao;
     private final Gson gson;
     protected final TemplateProcessor templateProcessor;
     private final Server server;
+    private final LoginService loginService;
 
-    public AdminServer(int port, UserDao userDao, Gson gson, TemplateProcessor templateProcessor) {
+    public AdminServer(LoginService loginService, int port, UserDao userDao, Gson gson, TemplateProcessor templateProcessor) {
+        this.loginService = loginService;
         this.userDao = userDao;
         this.gson = gson;
         this.templateProcessor = templateProcessor;
         ResourceHandler resourceHandler = createResourceHandler();
 
-        ServletContextHandler context = new ServletContextHandler();
-        context.addServlet(new ServletHolder(
-                new UsersApiServlet(userDao, gson)),
-                USER_API_CONTEXT_PATH);
-        context.addServlet(new ServletHolder(
-                new AdminTerminalServlet(templateProcessor, userDao)),
-                ADMIN_TERMINAL_CONTEXT_PATH);
+        ServletContextHandler context = createServletContextHandler();
 
         HandlerList handlers = new HandlerList();
         handlers.addHandler(resourceHandler);
-        handlers.addHandler(context);
+        handlers.addHandler(applySecurity(context, ADMIN_TERMINAL_CONTEXT_PATH, USER_API_CONTEXT_PATH));
         server = new Server(port);
         server.setHandler(handlers);
 
@@ -67,5 +75,41 @@ public class AdminServer implements UsersWebServer{
     @Override
     public void stop() throws Exception {
         server.stop();
+    }
+
+    protected Handler applySecurity(ServletContextHandler servletContextHandler, String... paths) {
+        Constraint constraint = new Constraint();
+        constraint.setName(CONSTRAINT_NAME);
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[]{ROLE_NAME_ADMIN});
+
+        List<ConstraintMapping> constraintMappings = new ArrayList<>();
+        Arrays.stream(paths).forEachOrdered(path -> {
+            ConstraintMapping mapping = new ConstraintMapping();
+            mapping.setPathSpec(path);
+            mapping.setConstraint(constraint);
+            constraintMappings.add(mapping);
+        });
+
+        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        //как декодировать стороку с юзером:паролем https://www.base64decode.org/
+        security.setAuthenticator(new BasicAuthenticator());
+
+        security.setLoginService(loginService);
+        security.setConstraintMappings(constraintMappings);
+        security.setHandler(new HandlerList(servletContextHandler));
+
+        return security;
+    }
+
+    private ServletContextHandler createServletContextHandler() {
+        ServletContextHandler context = new ServletContextHandler();
+        context.addServlet(new ServletHolder(
+                        new UsersApiServlet(userDao, gson)),
+                USER_API_CONTEXT_PATH);
+        context.addServlet(new ServletHolder(
+                        new AdminTerminalServlet(templateProcessor, userDao)),
+                ADMIN_TERMINAL_CONTEXT_PATH);
+        return context;
     }
 }
