cecb0446ee5f1e4295ea4a78f22b2d1ff5bce562
Ananev Konstantin <ananev@protei.ru>
2020-11-04 02:08:49 +0300

    implements clone, equals and hashCode for ObjectForMessage

--- hw07-lesson14-structural-patterns/src/main/java/ru/otus/ObjectForMessage.java
+++ hw07-lesson14-structural-patterns/src/main/java/ru/otus/ObjectForMessage.java
@@ -1,8 +1,10 @@
 package ru.otus;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class ObjectForMessage {
+public class ObjectForMessage implements Cloneable {
     private List<String> data;
 
     public List<String> getData() {
@@ -12,4 +14,33 @@ public class ObjectForMessage {
     public void setData(List<String> data) {
         this.data = data;
     }
+
+    @Override
+    protected ObjectForMessage clone() {
+        ObjectForMessage copy = new ObjectForMessage();
+        List<String> copyData = new ArrayList<>(data);
+        copy.setData(copyData);
+        return copy;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectForMessage{" +
+                "data=" + data.toString() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ObjectForMessage msg = (ObjectForMessage) obj;
+        return Objects.deepEquals(data, msg.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+
 }
